506500eb248af7f04806d0b2b1fe652ffb801ba2
Filipe <filipedtristao@gmail.com>
2020-06-21 02:02:33 -0300

    Create ConfigurationEnum

---
+++ app/Enums/ConfigurationEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self MIN_TEMPERATURE()
+ * @method static self MAX_TEMPERATURE()
+ */
+class ConfigurationEnum extends Enum
+{
+
+}
